163f847d41ad795f4c84634dc14b54351f007c48
Jamon27 <moiseev27@gmail.com>
2024-12-01 01:25:26 +0200

    Utilize controller with middleware

--- src/routes/chess.route.ts
+++ src/routes/chess.route.ts
@@ -1,20 +1,9 @@
-import express, { Request, Response } from 'express';
+import { Router } from 'express';
+import ChessController from '../controllers/chess.controller';
+import validateChessInput from '../middlewares/validation.middleware';
 
-const router = express.Router();
+const router = Router();
 
-router.post('/algorithm', async (req: Request, res: Response): Promise<any> => {
-  try {
-    const { inputData } = req.body;
-
-    if (!inputData) {
-      return res.status(400).json({ error: 'Input data is required' });
-    }
-
-    return res.status(200).json({ success: true });
-  } catch (error) {
-    console.error('Error executing algorithm:', error);
-    return res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+router.post('/paths', validateChessInput, ChessController.calculatePaths);
 
 export default router;
