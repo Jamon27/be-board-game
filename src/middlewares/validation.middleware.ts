d6357c1a863227e50cf4fe8057d4e37e575cc836
Jamon27 <moiseev27@gmail.com>
2024-12-01 09:37:37 +0200

    Add chess piece and steps types check

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -1,16 +1,43 @@
 import { Request, Response, NextFunction } from 'express';
 
+// todo:
+interface IChessRequest {
+  startPosition: string;
+  endPosition: string;
+  chessPieceType: string;
+  stepsLimit: number;
+}
+
 function validateChessInput(
   req: Request,
   res: Response,
   next: NextFunction,
 ): void {
-  const { start, end } = req.body;
+  const { startPosition, endPosition, chessPieceType, stepsLimit } = req.body;
 
-  if (!start || !end || !Array.isArray(start) || !Array.isArray(end)) {
+  if (
+    !startPosition ||
+    !endPosition ||
+    !(typeof startPosition === 'string') ||
+    !(typeof endPosition === 'string')
+  ) {
     res.status(400).json({
       message:
-        'Invalid input format. Expected start and end positions as arrays.',
+        'Invalid input format. Expected startPosition and endPosition as strings.',
+    });
+    return;
+  }
+
+  if (!chessPieceType || !(typeof chessPieceType === 'string')) {
+    res.status(400).json({
+      message: 'Invalid input format. Expected chessPieceType to be string.',
+    });
+    return;
+  }
+
+  if (!stepsLimit || !(typeof stepsLimit === 'number')) {
+    res.status(400).json({
+      message: 'Invalid input format. Expected stepsLimit to be number.',
     });
     return;
   }
