af4816f9b66899e8a1ebf164c17e6af93914c717
Jamon27 <moiseev27@gmail.com>
2024-12-01 15:11:31 +0200

    Add eslint skip rule

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -4,6 +4,7 @@ function errorMiddleware(
   err: Error,
   req: Request,
   res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction,
 ): void {
   console.error(err.stack);
