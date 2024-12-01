f71498cb62f70277e6d64f07d83e3bee1689db3e
Jamon27 <moiseev27@gmail.com>
2024-12-01 19:50:54 +0200

    Add cors headers

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,13 @@ const app = express();
 // Middleware to parse JSON
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 // Routes
 app.use('/api/chess', chessRoutes);
 app.use(errorMiddleware);
