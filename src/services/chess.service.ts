bb11b66f1950b83bb2edaa57e7636d35ee5a0a53
Jamon27 <moiseev27@gmail.com>
2024-12-01 10:30:20 +0200

    Implement calculatePath placeHolder

--- src/services/chess.service.ts
+++ src/services/chess.service.ts
@@ -1,5 +1,27 @@
+import { ChessPiece } from '../models/chess-piece.model';
+import { IChessboardMapper } from '../utils/chessboard-mapper.utils';
+
+interface ICalculatedPaths {
+  shortestPaths: string[];
+  allPaths: string[];
+}
+
 export class ChessService {
-  static calculatePath(): string[] {
-    return [];
+  private readonly chessboardMapper;
+  private chessPiece: ChessPiece;
+  constructor(chessPiece: ChessPiece, chessboardMapper: IChessboardMapper) {
+    this.chessboardMapper = chessboardMapper;
+    this.chessPiece = chessPiece;
+  }
+
+  calculatePath(endPosition: string, stepsLimit: number): ICalculatedPaths {
+    const end = this.chessboardMapper.parsePosition(endPosition);
+    if (end === null) {
+      throw new Error(
+        `Invalid end position "${end}". Must be in a1-h8 format.`,
+      );
+    }
+
+    return { shortestPaths: [], allPaths: [] };
   }
 }
