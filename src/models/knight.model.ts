b55c6d063262b9a3db1f615a1b8249c1eaff860b
Jamon27 <moiseev27@gmail.com>
2024-12-01 11:07:26 +0200

    Change move offsets and add chessboard util ref

--- src/models/knight.model.ts
+++ src/models/knight.model.ts
@@ -1,22 +1,27 @@
+import { IChessboardMapper } from '../utils/chessboard-mapper.utils';
 import { ChessPiece } from './chess-piece.model';
 
 export class Knight extends ChessPiece {
   private static moveOffsets = [
-    [2, 1],
-    [2, -1],
-    [-2, 1],
     [-2, -1],
-    [1, 2],
-    [1, -2],
-    [-1, 2],
+    [-2, 1],
     [-1, -2],
+    [-1, 2],
+    [1, -2],
+    [1, 2],
+    [2, -1],
+    [2, 1],
   ];
 
   protected getMoveOffsets(): number[][] {
     return Knight.moveOffsets;
   }
 
-  constructor(color: string, position: string) {
-    super(color, position);
+  constructor(
+    color: string,
+    position: string,
+    chessboardMapper: IChessboardMapper,
+  ) {
+    super(color, position, chessboardMapper);
   }
 }
